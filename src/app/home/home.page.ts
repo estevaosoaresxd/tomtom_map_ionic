4fd18ef3016d89d1378c22cd1406e30ea24d3216
Estevao4Innov <estevao.silva@4innovation.co>
2022-06-26 23:30:28 -0300

    main

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,69 @@
 import { Component } from '@angular/core';
-
+import tt from '@tomtom-international/web-sdk-maps';
+import { Geolocation, Position } from '@capacitor/geolocation';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  map: tt.Map;
+  center = { lng: -71.1167, lat: 42.377 };
+  reverseGeoCoded: any;
+  query: string = 'pizza';
+  searchResults: any;
+  currentLocationAvailable: boolean = false;
+  searchResultsAvailable: boolean = false;
+  searchResultMarker: any;
+  constructor(private http: HttpClient) {}
+  ionViewDidEnter() {
+    console.log('load');
 
-  constructor() {}
+    this.map = tt.map({
+      key: this.key,
+      container: 'map',
+      center: this.center,
+      zoom: 11,
+    });
 
+    let marker = new tt.Marker().setLngLat(this.center).addTo(this.map);
+  }
+  async getLocation() {
+    const coordinates: Position = await Geolocation.getCurrentPosition();
+    let marker2 = new tt.Marker({ color: 'green' })
+      .setLngLat([coordinates.coords.longitude, coordinates.coords.latitude])
+      .addTo(this.map);
+    this.getAddress(coordinates.coords);
+  }
+
+  async getAddress(home) {
+    const res: any = await this.http
+      .get(
+        `https://api.tomtom.com/search/2/reverseGeocode/${home.latitude}%2C${home.longitude}.json?key=${this.key}`
+      )
+      .toPromise();
+    this.reverseGeoCoded =
+      res.addresses[0].address.freeformAddress +
+      ' ' +
+      res.addresses[0].address.countryCodeISO3;
+  }
+
+  async search() {
+    const coordinates: Position = await Geolocation.getCurrentPosition();
+    const res: any = await this.http
+      .get(
+        `https://api.tomtom.com/search/2/search/${this.query}.json?lat=${coordinates.coords.latitude}&lon=${coordinates.coords.longitude}&key=${this.key}`
+      )
+      .toPromise();
+    this.searchResults = res.results;
+  }
+
+  locateResult(place) {
+    this.searchResultMarker = new tt.Marker({ color: 'orange' })
+      .setLngLat([place.position.lon, place.position.lat])
+      .addTo(this.map);
+    this.map.setCenter({ lng: place.position.lon, lat: place.position.lat });
+    this.map.setZoom(15);
+  }
 }
